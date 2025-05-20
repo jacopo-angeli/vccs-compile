ea4d55fb3ea76393a551b1fd88f7645de286c8f7
dipo-thinkpad <jacopo.angeli@studenti.unipd.it>
2025-05-20 15:48:13 +0200

    Cleaned repo and updated gitignore

--- src/Compiler.fs
+++ src/Compiler.fs
@@ -2,7 +2,11 @@ module Compiler
 open Vccs
 open Pccs
 
-let evaluateA (expression : Vccs.AExp) : int = failwith "Not Implemented"
+let evaluateA (expression : Vccs.AExp) : int = 
+    match expression with
+    | Num x -> x
+    | _ -> failwith "Not Implemented"
+
 let evaluateB (expression : Vccs.BExp) : bool = failwith "Not Implemented"
 let ty2val (ty : Vccs.ty) : int list = failwith "Not Implemented"
 
@@ -10,10 +14,15 @@ let compile (ccss: Vccs list) : Pccs list =
     // ! se trovo un input devo aver dichiarato la variabile che utilizzo nella dichiarazione iniziale ?
     // ! No check on redundancy
     let onlyvars = List.concat (List.map (fun (_, parameters, _) -> parameters) ccss)
+    printfn "%O" onlyvars
     let rec R (P : Vccs.P)= 
         match P with
         | Vccs.ConstCall(K,[]) -> 
-            let _, P  = match List.find (fun (id, _, _) -> id = K) ccss with _, args, body -> args, body
+            let _, P  = 
+                try
+                    match List.find (fun (id, _, _) -> id = K) ccss with _, args, body -> args, body
+                with | _ ->
+                    failwith "Process %s called before decalaration."
             Pccs.ConstCall(K)
 
         | Vccs.ConstCall(K, expressions) -> 
@@ -23,6 +32,7 @@ let compile (ccss: Vccs list) : Pccs list =
 
         | Vccs.Input(channel, variable, P) ->
             // Serve il tipo di variabile
+            printfn "Input action"
             let _, ty = List.find (fun (id, _) -> id = variable ) onlyvars
             let ccsinputs = List.map (fun v -> Pccs.Input(sprintf "%s_%d" channel v, R (Vccs.Rename(P, [RenameVal(v, variable)])))) (ty2val ty)
             List.reduce (fun x y -> Pccs.Sum(x,y))  ccsinputs
@@ -53,4 +63,5 @@ let compile (ccss: Vccs list) : Pccs list =
         | Vccs.Nil ->
             Pccs.Nil
     
+    printfn "%O" ccss
     List.map (fun (K, _, P) -> Pccs(K, R P)) ccss
